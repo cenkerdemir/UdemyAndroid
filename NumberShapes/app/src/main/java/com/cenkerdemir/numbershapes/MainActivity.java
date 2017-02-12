4f0d1673f07e998958bb18140c5e1a06afc400cd
github username <cenkerdemir@yahoo.com>
2017-02-12 16:43:16 +0300

    numbershapes code finalized

--- NumberShapes/app/src/main/java/com/cenkerdemir/numbershapes/MainActivity.java
+++ NumberShapes/app/src/main/java/com/cenkerdemir/numbershapes/MainActivity.java
@@ -2,37 +2,68 @@ package com.cenkerdemir.numbershapes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void numberGoButtonClicked(View view) {
-        EditText numberToCheckAsText = (EditText) findViewById(R.id.numberEditText);
+    class Number {
 
-       Integer numberToCheckAsInteger = Integer.parseInt(numberToCheckAsText.getText().toString());
+        Integer number = 0;
 
-        Number numberToCheck = new Number();
-        numberToCheck.number = numberToCheckAsInteger;
+        public boolean isTriangular(){
+            Integer x = 1;
+            while (x*(x+1) <= number*2) {
+                if (x*(x+1) == number*2) {
+                    return true;
+                }
+                x++;
+            }
+            return false;
+        }
 
-        if (numberToCheck.isSquare() && numberToCheck.isTriangle()) {
-            this.setTextForTextView("Your number is both triangular and square");
-        }
-        else if (numberToCheck.isSquare()) {
-            this.setTextForTextView("Your number is a square number");
-        }
-        else if (numberToCheck.isTriangle()) {
-            this.setTextForTextView("Your number is a triangular number");
-        }
-        else {
-            this.setTextForTextView("Your number is neither triangular nor square");
+        public boolean isSquare(){
+            Integer x = 1;
+            while (x*x <= number) {
+                if (x*x == number) {
+                    return true;
+                }
+                x++;
+            }
+            return false;
         }
     }
 
-    public void setTextForTextView(String text) {
-        TextView tv = (TextView) findViewById(R.id.resultLabel);
-        tv.setText(text);
+    public void numberGoButtonClicked(View view) {
+        String message = "";
+
+        EditText numberToCheckAsText = (EditText) findViewById(R.id.numberEditText);
+
+        try {
+            Integer numberToCheckAsInteger = Integer.parseInt(numberToCheckAsText.getText().toString());
+
+            Number numberToCheck = new Number();
+            numberToCheck.number = numberToCheckAsInteger;
+
+            if (numberToCheck.isSquare() && numberToCheck.isTriangular()) {
+                message = "Your number is both triangular and square";
+            } else if (numberToCheck.isSquare()) {
+                message = "Your number is a square number";
+            } else if (numberToCheck.isTriangular()) {
+                message = "Your number is a triangular number";
+            } else {
+                message = "Your number is neither triangular nor square";
+            }
+        }
+        catch (Exception e) {
+            message = "Please enter a number";
+            Log.i("error occured", e.getMessage());
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -40,5 +71,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
 
