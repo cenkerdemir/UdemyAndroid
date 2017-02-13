1709d7211d0657bad74396a71279f48b79d4417a
github username <cenkerdemir@yahoo.com>
2017-02-13 22:18:45 +0300

    video demo app done

--- VideoDemo/app/src/main/java/com/cenkerdemir/videodemo/MainActivity.java
+++ VideoDemo/app/src/main/java/com/cenkerdemir/videodemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.cenkerdemir.videodemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView oceanVideoView = (VideoView) findViewById(R.id.waterVideoView);
+        oceanVideoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.water);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(oceanVideoView);
+
+        oceanVideoView.setMediaController(mediaController);
+
+        oceanVideoView.start();
     }
 }
