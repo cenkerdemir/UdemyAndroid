d27984823d1b808e6911810822f0565667c82de3
github username <cenkerdemir@yahoo.com>
2017-02-16 10:36:23 +0300

    grid layout demo app done

--- GridLayoutDemo/app/src/main/java/com/cenkerdemir/gridlayoutdemo/MainActivity.java
+++ GridLayoutDemo/app/src/main/java/com/cenkerdemir/gridlayoutdemo/MainActivity.java
@@ -1,9 +1,22 @@
 package com.cenkerdemir.gridlayoutdemo;
 
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void buttonClicked(View view) {
+        int id = view.getId();
+        String ourId = "";
+        ourId = getResources().getResourceEntryName(id);
+        int resourceId = getResources().getIdentifier(ourId, "raw", getPackageName());
+
+        MediaPlayer mPlayer = MediaPlayer.create(this, resourceId);
+        mPlayer.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
